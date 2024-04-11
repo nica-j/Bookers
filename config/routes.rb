e7a1564e022c9df960917ce281fdb3135d131ca3
EC2 Default User <ec2-user@ip-172-31-1-143.ap-northeast-1.compute.internal>
2024-04-11 20:03:36 +0000

    [finish] top

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'books/show'
   get 'books/edit'
   get 'homes/top'
+  # urlの変更ってどうやる？
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
